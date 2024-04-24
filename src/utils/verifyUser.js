4e5f80ac3d2b68ca9c928eca0d27433f73843ea6
Md-Abdullah-321 <abdullah.dev.it@gmail.com>
2024-04-25 05:34:12 +0600

    trying to upload on vercel

--- src/utils/verifyUser.js
+++ src/utils/verifyUser.js
@@ -2,6 +2,14 @@ import jwt from "jsonwebtoken";
 import { errorHandler } from "./error.js";
 
 export const verifyToken = (req, res, next) => {
+    // Check if the request originates from your client application
+    const allowedOrigin = 'https://real-estate-client-tau.vercel.app';
+    if (req.headers.origin === allowedOrigin) {
+        // Allow unrestricted access for requests from the client
+        return next();
+    }
+
+    // For requests from other origins, continue with token verification
     const token = req.cookies.access_token;
     if (!token) {
         return next(errorHandler(401, 'Unauthorized'));
