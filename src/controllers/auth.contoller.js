75aeadcfbcc652f4b6ebd802b0ebe4321d80139d
Md-Abdullah-321 <abdullah.dev.it@gmail.com>
2024-04-25 03:26:58 +0600

    fixed login issue

--- src/controllers/auth.contoller.js
+++ src/controllers/auth.contoller.js
@@ -31,32 +31,30 @@ export const signup = async(req, res, next) => {
 
 export const signin = async (req, res, next) => {
     const { email, password } = req.body;
-
-   
+    
     try {
         const validUser = await User.findOne({ email });
-
+        
         if (!validUser) {
             return next(errorHandler(404, 'Wrong credentials!'));
         }
-
-        const validPassword = bcryptjs.compareSync(password, validUser.password);
-
-        if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
         
+        const validPassword = bcryptjs.compareSync(password, validUser.password.trim());
+
+        if (!validPassword) {
+            return next(errorHandler(401, 'Wrong credentials!'));
+        }
 
         const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
 
         const expirationDate = new Date();
         expirationDate.setTime(expirationDate.getTime() + 24 * 60 * 60 * 1000);
 
-
-        const { password:pass , ...rest } = validUser._doc;
+        const { password: pass, ...rest } = validUser._doc;
 
         res.cookie('access_token', token, { httpOnly: true, expires: expirationDate })
-        .status(200)
-        .json(rest);
-
+            .status(200)
+            .json(rest);
     } catch (error) {
         next(errorHandler(400, 'Could not find user.'));
     }
