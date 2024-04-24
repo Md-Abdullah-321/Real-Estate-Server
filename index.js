c64709f2cafe5dd64ab567ede9741f7affb07286
Md-Abdullah-321 <abdullah.dev.it@gmail.com>
2024-04-24 22:56:27 +0600

    changed file structure

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
-import authRouter from './routes/auth.route.js';
-import listingRouter from './routes/listing.route.js';
-import userRouter from './routes/user.route.js';
+import authRouter from './src/routes/auth.route.js';
+import listingRouter from './src/routes/listing.route.js';
+import userRouter from './src/routes/user.route.js';
 
 dotenv.config();
 const app = express();
