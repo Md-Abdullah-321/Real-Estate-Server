f83f2390fa2a159c8868e3f322654c2b069f2558
Md-Abdullah-321 <abdullah.dev.it@gmail.com>
2024-04-25 06:51:39 +0600

    updated auth

--- src/controllers/auth.contoller.js
+++ src/controllers/auth.contoller.js
@@ -14,51 +14,49 @@ import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
 
 
-export const signup = async(req, res, next) => {
-    const {username, email, password} = req.body;
+export const signup = async (req, res, next) => {
+    const { username, email, password } = req.body;
 
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({ username, email, password: hashedPassword });
 
     try {
         await newUser.save();
-        res.status(201).json({message: 'user created successfully'})
+        res.status(201).json({ message: 'User created successfully' });
     } catch (error) {
-        next(errorHandler(550, 'Could not create new user.'));
+        next(errorHandler(500, 'Could not create new user.'));
     }
-}
-
+};
 
 export const signin = async (req, res, next) => {
     const { email, password } = req.body;
-    
+
     try {
         const validUser = await User.findOne({ email });
-        
+
         if (!validUser) {
             return next(errorHandler(404, 'Wrong credentials!'));
         }
-        
-        const validPassword = bcryptjs.compareSync(password, validUser.password.trim());
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
 
         if (!validPassword) {
             return next(errorHandler(401, 'Wrong credentials!'));
         }
 
-        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
-
-        const expirationDate = new Date();
-        expirationDate.setTime(expirationDate.getTime() + 24 * 60 * 60 * 1000);
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, {
+            expiresIn: '24h', // Token expires in 24 hours
+        });
 
         const { password: pass, ...rest } = validUser._doc;
 
-        res.cookie('access_token', token, { httpOnly: true, expires: expirationDate })
+        res.cookie('access_token', token, { httpOnly: true, expires: new Date(Date.now() + 24 * 60 * 60 * 1000) })
             .status(200)
             .json(rest);
     } catch (error) {
         next(errorHandler(400, 'Could not find user.'));
     }
-}
+};
 
 
 
